29b80b770246104d407130f8f0d44de0e8bbe1a7
Michal Czarnik <michal.czarnik@outlook.com>
2016-06-13 20:23:30 +0200

    Added multithreded server handling

--- MichalCzarnik/server.py
+++ MichalCzarnik/server.py
@@ -1,13 +1,22 @@
 import socket
 import _thread
 import time
+import threading
+import sys
 
+i=1
 # Define a function for the thread
-def print_time( threadName):
-    c, addr = s.accept()
+def print_time( c, addr):
     print('Connected from ',addr)
+    global i
+    print(i)
+    i+=1
     c.send('Thanks for connecting to server'.encode())
     c.close()
+    time.sleep(5)
+    print(i)
+    print('Connection Closed ',addr)
+
 
 s=socket.socket()
 host = socket.gethostname()
@@ -16,10 +25,14 @@ s.bind((host, port))
 
 s.listen(5)
 while True:
+    c, addr = s.accept()
     try:
-        _thread.start_new_thread( print_time, ("Thread-1",  ) )
+        t=threading.Thread(target=print_time, args=(c, addr))
+        t.daemon=True
+        t.start()
+        # _thread.start_new_thread( print_time, (c, addr ) )
     except:
-        print ("Error: unable to start thread")
+        print ("Error: "+str(sys.exc_info()))
 
 while 1:
    pass
